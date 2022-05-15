7806e6937290100d9baa3002f4f4ea9d7bf36d07
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-05-15 23:28:04 +0800

    test vscode
    test vscode java project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+            Scene scene = new Scene(root, 300, 250);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
