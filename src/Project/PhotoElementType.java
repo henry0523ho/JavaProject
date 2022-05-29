f701c52e174e4b10b66cccd80b75307d49ba32df
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-05-29 19:29:38 +0800

    Update PhotoElementType.java

--- src/Project/PhotoElementType.java
+++ src/Project/PhotoElementType.java
@@ -10,4 +10,11 @@ public class PhotoElementType {
         typeName=name;
         photoElement=new ArrayList<PhotoElement>();
     }
+
+    public String getTypeName() {
+        return typeName;
+    }
+    public void setTypeName(String typeName) {
+        this.typeName=typeName;
+    }
 }
